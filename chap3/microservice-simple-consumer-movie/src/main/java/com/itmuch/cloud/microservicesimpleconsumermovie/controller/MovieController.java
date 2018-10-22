119a73c52a5b56d4f1620f55a155d8574dbb50de
belliwu <belliwu56@gmail.com>
2018-10-22 08:56:39 +0800

    delete user project

--- chap3/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
+++ chap3/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MovieController.java
@@ -1,6 +1,10 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +12,25 @@ import org.springframework.web.client.RestTemplate;
 
 import com.itmuch.cloud.microservicesimpleconsumermovie.model.User;
 
+
 @RestController
 public class MovieController
 {
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private DiscoveryClient client;
 
 	@GetMapping("/user/{id}")
 	public User findById(@PathVariable Long id)
 	{
 		return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+	}
+	
+	@GetMapping("/user-instance")
+	public List<ServiceInstance> showInfo()
+	{
+		return this.client.getInstances("microservice-provider-user");
 	}
 }
